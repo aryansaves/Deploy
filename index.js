3b09213c32b97049f47d53b1263bb58c9c8f60a7
aryan7905 <aryansrivastava354@gmail.com>
2026-02-04 14:14:43 +0530

    for sir satidfaction

---
+++ index.js
@@ -0,0 +1,26 @@
+import fs from "fs"
+import http from "http"
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/data' && req.method === 'GET'){
+        fs.readFile('./data.json', 'utf-8', (err, data) => {
+            if(err){
+                res.writeHead(500, {'Content-Type': 'application/json'})
+                res.end(JSON.stringify({error: 'Failed to read data'}))
+                return 
+            }
+            
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            res.end(data)
+    })
+    } else {
+        
+        res.writeHead(404, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify({error: 'Not found'}))
+    }
+})
+
+const PORT = 3000
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
